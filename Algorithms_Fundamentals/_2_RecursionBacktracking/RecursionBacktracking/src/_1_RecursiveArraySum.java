df4839caee8b0faefac31513128ae9bf87cd4124
Aleatmos <a.moskov95@gmail.com>
2023-06-04 09:58:32 +0300

    solution to problems

---
+++ Algorithms_Fundamentals/_2_RecursionBacktracking/RecursionBacktracking/src/_1_RecursiveArraySum.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class _1_RecursiveArraySum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] arr = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        int sum = 0;
+
+        sum = sumNumber(arr, arr.length - 1);
+
+        System.out.println("Iteration sum: " + sum);
+    }
+
+    public static int sumNumber(int[] numbers, int index) {
+
+        if (index >= numbers.length) {
+            return 0;
+        }
+
+        return numbers[index] + sumNumber(numbers, index + 1);
+    }
+}
