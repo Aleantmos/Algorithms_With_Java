c0ffb5ecc2d5e0d2d57a2f5cd8f5d490c666d76b
Aleatmos <a.moskov95@gmail.com>
2023-06-06 10:05:35 +0300

    solutions

--- Algorithms_Fundamentals/_5_SearchingSortingGreedyAlrorithms/SearchingSortingGreedyAlrorithms/src/_3_BubbleSort.java
+++ Algorithms_Fundamentals/_5_SearchingSortingGreedyAlrorithms/SearchingSortingGreedyAlrorithms/src/_3_BubbleSort.java
@@ -12,7 +12,7 @@ public class _3_BubbleSort {
     public static void sort(int[] arr) {
 
         for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr.length - 1; j++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
                     swap(arr, j, j + 1);
                 }
