a430a124d0278248c4d5d74d73beb4641ad1480f
Aleatmos <a.moskov95@gmail.com>
2023-06-10 14:03:29 +0300

    depth for search

---
+++ Algorithms_Fundamentals/_6_GraphTraversalShortestPath/05. Graph-Theory-Traversal-and-Shortest-Paths-Skeleton/src/main/java/Main.java
@@ -0,0 +1,70 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int n = Integer.parseInt(scan.nextLine());
+
+        List<List<Integer>> graph = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            String input = scan.nextLine();
+
+            if (input.trim().equals("")) {
+                graph.add(new ArrayList<>());
+            } else {
+                List<Integer> nextNodes = Arrays.stream(input.split("\\s+"))
+                        .map(Integer::parseInt)
+                        .collect(Collectors.toList());
+                graph.add(nextNodes);
+            }
+        }
+
+        List<Deque<Integer>> connectedComponents = getConnectedComponents(graph);
+
+        for (Deque<Integer> connectedComponent : connectedComponents) {
+            System.out.print("Connected component: ");
+            for (Integer integer : connectedComponent) {
+                System.out.println(integer + " ");
+            }
+        }
+
+    }
+
+    public static List<Deque<Integer>> getConnectedComponents(List<List<Integer>> graph) {
+        boolean[] visited = new boolean[graph.size()];
+
+        List<Deque<Integer>> components = new ArrayList<>();
+
+        for (int start = 0; start < graph.size(); start++) {
+            if (!visited[start]) {
+                components.add(new ArrayDeque<>());
+                dfs(start, components, graph, visited);
+            }
+        }
+
+        return components;
+    }
+
+    private static void dfs(int node,
+                            List<Deque<Integer>> components,
+                            List<List<Integer>> graph,
+                            boolean[] visited) {
+        if (!visited[node]) {
+            visited[node] = true;
+
+            for (int child : graph.get(node)) {
+                dfs(child, components, graph, visited);
+            }
+            components.get(components.size() - 1).offer(node);
+        }
+
+    }
+
+
+    public static Collection<String> topSort(Map<String, List<String>> graph) {
+        throw new AssertionError("Not Implemented");
+    }
+}
---
+++ Algorithms_Fundamentals/_6_GraphTraversalShortestPath/05. Graph-Theory-Traversal-and-Shortest-Paths-Skeleton/src/test/java/ConnectedComponentsTests.java
@@ -0,0 +1,12 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.*;
+
+public class ConnectedComponentsTests {
+
+    @Test
+    public void TestGraphConnectedComponents9Vertices() {
+
+    }
+}
