10e75ade88951e4002ee6269c91d4bab380406b6
Aleatmos <a.moskov95@gmail.com>
2023-06-30 12:55:29 +0300

    solution

--- Algorithms_Fundamentals/_8_DynamicProgrammingIntro/DynamicProgramming/src/_2_LongestIncreasingSubsequence.java
+++ Algorithms_Fundamentals/_8_DynamicProgrammingIntro/DynamicProgramming/src/_2_LongestIncreasingSubsequence.java
@@ -8,48 +8,49 @@ public class _2_LongestIncreasingSubsequence {
 
         Scanner scan = new Scanner(System.in);
 
-        int[] sequence = Arrays.stream(scan.nextLine().split("\\s+"))
-                .mapToInt(Integer::parseInt)
-                .toArray();
+        int[] sequence = { 3, 14, 5, 12, 15, 7, 8, 9, 11, 10, 1};
 
         int[] length = new int[sequence.length];
         int[] prev = new int[sequence.length];
 
-        Arrays.fill(prev, -1);;
+        Arrays.fill(prev, -1);
 
         int maxLength = 0;
-        int maxIndex = -1;
+        int maxIndex = 0;
 
         for (int i = 0; i < sequence.length; i++) {
-            int curr = sequence[i];
+            int current = sequence[i];
             int bestLength = 1;
-            int bestIndex = -1;
+            int bestIndex = - 1;
 
-            for (int j = i - 1; j >= 0; j-- ) {
-                if (sequence[j] < curr && length[j] + 1 >= bestLength) {
+            for (int j = i - 1; j >= 0; j--) {
+                if (sequence[j] < current && length[j] + 1 > bestLength) {
                     bestLength = length[j] + 1;
                     bestIndex = j;
                 }
             }
+
             prev[i] = bestIndex;
             length[i] = bestLength;
-            if (maxLength < bestLength) {
+
+            if (maxLength <= bestLength) {
                 maxLength = bestLength;
                 maxIndex = i;
             }
         }
+
         List<Integer> LIS = new ArrayList<>();
-        LIS.add(sequence[maxIndex]);
+
 
         int index = maxIndex;
 
         while (index != -1) {
-            LIS.add(sequence[index]);
-            index = prev[index];
+            LIS.add(sequence[maxIndex]);
+            index = prev[maxIndex];
         }
 
         for (int i = LIS.size() - 1; i >= 0; i--) {
-            System.out.print(LIS.get(i) + " ");
+            System.out.println(LIS.get(i) + " ");
         }
     }
 }
