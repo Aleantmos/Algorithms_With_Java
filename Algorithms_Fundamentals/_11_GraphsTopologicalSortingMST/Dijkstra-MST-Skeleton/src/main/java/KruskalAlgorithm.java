87b6885a003a343a5c202ae0034f63450220540b
Aleatmos <a.moskov95@gmail.com>
2023-07-11 13:31:20 +0300

    kruskal and prim

--- Algorithms_Fundamentals/_11_GraphsTopologicalSortingMST/Dijkstra-MST-Skeleton/src/main/java/KruskalAlgorithm.java
+++ Algorithms_Fundamentals/_11_GraphsTopologicalSortingMST/Dijkstra-MST-Skeleton/src/main/java/KruskalAlgorithm.java
@@ -2,6 +2,71 @@ import java.util.*;
 
 public class KruskalAlgorithm {
 
+    public static Set<Integer> visited = new HashSet<>();
+    public static Map<Integer, List<Edge>> edgesByNode = new HashMap<>();
+
+    public static List<Edge> kruskal(int numberOfVertices, List<Edge> edges) {
+        List<Edge> forest = new ArrayList<>();
+
+        for (Edge edge : edges) {
+            edgesByNode.putIfAbsent(edge.getStartNode(), new ArrayList<>());
+            edgesByNode.get(edge.getStartNode()).add(edge);
+
+            edgesByNode.putIfAbsent(edge.getEndNode(), new ArrayList<>());
+            edgesByNode.get(edge.getEndNode()).add(edge);
+        }
+
+        for (int node : edgesByNode.keySet()) {
+            if (!visited.contains(node)) {
+                prim(node, forest);
+            }
+        }
+
+        forest.sort(Comparator.comparingInt(Edge::getWeight));
+
+        return forest;
+    }
+
+    private static void prim(int start, List<Edge> forest) {
+        visited.add(start);
+
+        PriorityQueue<Edge> edges = new PriorityQueue<>(Comparator.comparingInt(Edge::getWeight));
+
+        edges.addAll(edgesByNode.get(start));
+
+        while (!edges.isEmpty()) {
+            Edge minEdge = edges.poll();
+
+            int sourceNode = minEdge.getStartNode();
+            int destinationNode = minEdge.getStartNode();
+
+            int nonTreeNode = -1;
+
+            if (visited.contains(sourceNode) && !visited.contains(destinationNode)) {
+                nonTreeNode = destinationNode;
+            }
+
+            if (visited.contains(destinationNode) && !visited.contains(sourceNode)) {
+                nonTreeNode = sourceNode;
+            }
+
+            if (nonTreeNode != -1) {
+                forest.add(minEdge);
+                visited.add(nonTreeNode);
+                edges.addAll(edgesByNode.get(nonTreeNode));
+            }
+        }
+    }
+
+
+
+
+    /*
+
+            -----------------KRUSKAL-----------------
+
+
+
     public static List<Edge> kruskal(int numberOfVertices, List<Edge> edges) {
 
         Collections.sort(edges);
@@ -14,6 +79,13 @@ public class KruskalAlgorithm {
             parents[i] = i;
         }
 
+
+
+
+
+
+
+
         while (!edges.isEmpty()) {
             Edge edge = edges.remove(0);
 
@@ -25,20 +97,17 @@ public class KruskalAlgorithm {
 
             if (firstRoot != secondRoot) {
                 forest.add(edge);
-                parents[firstRoot] = secondRoot ;
+                parents[firstRoot] = secondRoot;
             }
-
-
         }
-        return null;
+        return forest;
     }
 
     public static int findRoot(int node, int[] parents) {
 
-
         while (parents[node] != node) {
             node = parents[node];
         }
-        return 0;
-    }
+        return node;
+    }*/
 }
