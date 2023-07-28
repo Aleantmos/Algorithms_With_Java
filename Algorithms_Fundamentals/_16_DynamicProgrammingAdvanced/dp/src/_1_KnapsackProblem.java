c71d410d7eb5290351d2af79ce9cdaba35843063
Aleatmos <a.moskov95@gmail.com>
2023-07-28 12:38:18 +0300

    memorizatsia

--- Algorithms_Fundamentals/_16_DynamicProgrammingAdvanced/dp/src/_1_KnapsackProblem.java
+++ Algorithms_Fundamentals/_16_DynamicProgrammingAdvanced/dp/src/_1_KnapsackProblem.java
@@ -1,10 +1,12 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 public class _1_KnapsackProblem {
     public static List<Integer> weights = new ArrayList<>();
     public static List<Integer> prices = new ArrayList<>();
+    public static int[][] dp;
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
@@ -23,6 +25,12 @@ public class _1_KnapsackProblem {
             line = scan.nextLine();
         }
 
+        dp = new int[prices.size() + 1][capacity + 1];
+
+        for (int i = 0; i < dp.length; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+
         int result = recurrence(0, 0, capacity);
 
         System.out.println(result);
@@ -34,7 +42,11 @@ public class _1_KnapsackProblem {
             return 0;
         }
 
-        return Math.max(recurrence(valueIndex + 1, weightIndex + 1, capacity),
+        if (dp[valueIndex][capacity] != -1) {
+            return dp[valueIndex][capacity];
+        }
+
+        return dp[valueIndex][capacity] = Math.max(recurrence(valueIndex + 1, weightIndex + 1, capacity),
                 recurrence(valueIndex + 1, weightIndex + 1, capacity - weights.get(weightIndex)
                         + prices.get(valueIndex)));
     }
