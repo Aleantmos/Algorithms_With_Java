d267316bfeb64e8579d37b697c84fb6bd36db804
Aleatmos <a.moskov95@gmail.com>
2023-07-30 16:59:52 +0300

    other solution

--- Algorithms_Fundamentals/_5_SearchingSortingGreedyAlrorithms/SearchingSortingGreedyAlrorithms/src/_2_SelectionSorting.java
+++ Algorithms_Fundamentals/_5_SearchingSortingGreedyAlrorithms/SearchingSortingGreedyAlrorithms/src/_2_SelectionSorting.java
@@ -17,6 +17,69 @@ public class _2_SelectionSorting {
     }
 
     private static void sort(int[] arr) {
+        int inputLength = arr.length;
+
+        if (inputLength < 2) {
+            return;
+        }
+        int midIndex = inputLength / 2;
+
+        int[] left = new int[midIndex];
+        int[] right = new int[inputLength - midIndex];
+
+        for (int i = 0; i < midIndex; i++) {
+            left[i] = arr[i];
+        }
+
+        for (int i = midIndex; i < inputLength; i++) {
+            right[i - midIndex] = arr[i];
+        }
+
+        sort(left);
+        sort(right);
+
+        merge(arr, left, right);
+    }
+
+    private static void merge(int[] arr, int[] left, int[] right) {
+        int leftSize = left.length;
+        int rightSize = right.length;
+
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        while (i < leftSize && j < rightSize) {
+            if (left[i] <= right[j]) {
+                arr[k] = left[i];
+                i++;
+            } else  {
+                arr[k] = right[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < leftSize) {
+            arr[k] = left[i];
+            i++;
+            k++;
+        }
+
+        while (j < rightSize) {
+            arr[k] = left[j];
+            j++;
+            k++;
+        }
+
+
+
+    }
+}
+
+
+
+    /*private static void sort(int[] arr) {
         for (int i = 0; i < arr.length ; i++) {
             int min = i;
             for (int j = min + 1; j < arr.length; j++) {
@@ -34,5 +97,5 @@ public class _2_SelectionSorting {
         int temp = arr[i];
         arr[i] = arr[min];
         arr[min] = temp;
-    }
-}
+    }*/
+
