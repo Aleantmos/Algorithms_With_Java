69aa6aca410f9920636dddaec6c3e8c494190bab
Aleatmos <a.moskov95@gmail.com>
2023-08-12 20:01:51 +0300

    bugs found

--- Algorithms_Fundamentals/_10_ExamPrep/ExamPrep/src/_1_Trains.java
+++ Algorithms_Fundamentals/_10_ExamPrep/ExamPrep/src/_1_Trains.java
@@ -7,32 +7,31 @@ public class _1_Trains {
 
         double[] arrivals = Arrays.stream(scan.nextLine().split("\\s+"))
                 .mapToDouble(Double::parseDouble)
+                .sorted()
                 .toArray();
 
         double[] departures = Arrays.stream(scan.nextLine().split("\\s+"))
                 .mapToDouble(Double::parseDouble)
+                .sorted()
                 .toArray();
-        
 
-        Arrays.sort(departures);
-
-        int platforms = 0;
+        int platformsCnt = 0;
         int maxPlatforms = 0;
+
 
         for (int i = 0, j = 0; i < arrivals.length; i++) {
             if (arrivals[i] < departures[j]) {
-                platforms++;
-                i++;
+                platformsCnt++;
 
-                if (platforms > maxPlatforms) {
-                    maxPlatforms = platforms;
+                if (platformsCnt > maxPlatforms) {
+                    maxPlatforms = platformsCnt;
                 }
-
             } else {
-                platforms--;
                 j++;
             }
         }
+
+        System.out.println(maxPlatforms);
 
     }
 }
