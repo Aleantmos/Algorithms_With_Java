837126463a3685c021c2757e417c133b75bcbd94
Aleatmos <a.moskov95@gmail.com>
2023-08-12 20:39:01 +0300

    bugs

--- Algorithms_Fundamentals/_10_ExamPrep/ExamPrep/src/_2_Stairs.java
+++ Algorithms_Fundamentals/_10_ExamPrep/ExamPrep/src/_2_Stairs.java
@@ -2,41 +2,38 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class _2_Stairs {
-    private static long[] memory;
-
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        int stairs = Integer.parseInt(scan.nextLine());
+        int steps = Integer.parseInt(scan.nextLine());
 
 
-        if (stairs == 0) {
-            System.out.println(0);
-            return;
-        }
-        memory = new long[stairs + 1];
+        int[] memory = new int [steps + 1];
 
-        Arrays.fill(memory, -1);
+        Arrays.fill(memory, 0);
 
-        System.out.println(climbStares(stairs));
+        memory[1] = 1;
+        memory[2] = 2;
+
+        System.out.println(climbSteps(steps, memory));
+
     }
 
-    private static long climbStares(int stairs) {
-        if (memory[stairs] != 0) {
-            return memory[stairs];
+    private static int climbSteps(int steps, int[] memory) {
+        if (memory[steps] != 0) {
+            return memory[steps];
         }
-
-        if (stairs == 1) {
+        if (steps == 1) {
             return 1;
         }
 
-        if (stairs == 2) {
+        if (steps == 2) {
             return 2;
         }
 
-        long result = climbStares(stairs - 1) + climbStares(stairs - 2);
+        int result = climbSteps(steps - 1, memory) + climbSteps(steps - 2, memory);
 
-        memory[stairs] = result;
+        memory[steps] = result;
 
         return result;
     }
