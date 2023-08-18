6d4227e5cd0f7f25ba448d2e1c2f61d9a6c5e75e
Aleatmos <a.moskov95@gmail.com>
2023-08-18 13:32:03 +0300

    not working

--- Algorithms_Fundamentals/_10_ExamPrep/OtherExams/28_06_2020/exam/src/_3_BlackMesa.java
+++ Algorithms_Fundamentals/_10_ExamPrep/OtherExams/28_06_2020/exam/src/_3_BlackMesa.java
@@ -1,11 +1,8 @@
 import java.util.*;
 
 public class _3_BlackMesa {
-    //not working - redo
-    private static List<Integer> visitedNodes = new ArrayList<>();
     private static boolean[] isVisited;
     private static List<Integer> path = new ArrayList<>();
-    private static final int cnt = 0;
     private static boolean isFound = false;
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
@@ -13,7 +10,6 @@ public class _3_BlackMesa {
         Map<Integer, Set<Integer>> graph = new HashMap<>();
 
         int nodes = Integer.parseInt(scan.nextLine());
-
         int edges = Integer.parseInt(scan.nextLine());
 
         for (int i = 0; i < nodes; i++) {
@@ -23,18 +19,20 @@ public class _3_BlackMesa {
         isVisited = new boolean[nodes + 1];
 
         for (int i = 0; i < edges; i++) {
-            String[] edge = scan.nextLine().split(" ");
+            String[] edge = scan.nextLine().split("\\s+");
             int from = Integer.parseInt(edge[0]);
             int to = Integer.parseInt(edge[1]);
-
             graph.get(from).add(to);
         }
 
-        int startNode = Integer.parseInt(scan.nextLine().trim());
-        int destination = Integer.parseInt(scan.nextLine().trim());
+        int source = Integer.parseInt(scan.nextLine().trim());
+        int dest = Integer.parseInt(scan.nextLine().trim());
 
+        isVisited[source] = true;
 
-        dfs(graph, startNode, destination);
+        path.add(source);
+
+        dfs(graph, source, dest);
 
         StringBuilder result = new StringBuilder();
         for (Integer curr : path) {
@@ -52,25 +50,33 @@ public class _3_BlackMesa {
         }
 
         System.out.println(result.toString().trim());
-
     }
 
-    private static void dfs(Map<Integer, Set<Integer>> graph, int current, int destination) {
+    private static void dfs(Map<Integer, Set<Integer>> graph, int current, int dest) {
+        //path.add(current);
+
+        /*if (current == dest) {
+            isFound = true;
+            return;
+        }*/
+
         Set<Integer> linkedNodes = graph.get(current);
-        isVisited[current] = true;
-        if (!isFound) {
-            path.add(current);
-            if (current == destination) {
-                isFound = true;
-            }
-        }
 
-        for (Integer linkedNode : linkedNodes) {
-            if (!isVisited[linkedNode]) {
-                visitedNodes.add(current);
-
-                dfs(graph, linkedNode, destination);
+        for (Integer childNode : linkedNodes) {
+            if (!isVisited[childNode]) {
+                isVisited[childNode] = true;
+                if (childNode == dest && !isFound) {
+                    path.add(childNode);
+                    isFound = true;
+                } else if (!isFound){
+                    path.add(childNode);
+                }
+                dfs(graph, childNode, dest);
+                if (!isFound && isVisited[childNode]) {
+                    path.remove(path.size() - 1);
+                }
             }
         }
     }
+
 }
